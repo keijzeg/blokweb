ed74a734ccd4419d99ab34d1888fcc57539ceb02
keijzeg <74183639+keijzeg@users.noreply.github.com>
2020-12-11 13:47:40 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+var latestButton = document.querySelector('#latestknop') ;
+var matchesButton = document.querySelector('#matchesknop');
+
+
+// eventHandler functies
+function weergeefLatest(  ) {
+	document.querySelector('#latestknop').classList.add('active');
+	document.querySelector('#matchesknop').classList.remove('active');
+}
+function weergeefMatches(  ) {
+	document.querySelector('#matchesknop').classList.add('active');
+	document.querySelector('#latestknop').classList.remove('active');
+}
+
+
+// eventListeners
+latestButton.addEventListener('click', weergeefLatest);
+matchesButton.addEventListener('click', weergeefMatches);
