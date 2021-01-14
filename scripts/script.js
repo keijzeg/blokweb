46e38c0c5b58dd649c153ee20ea44835a3a79f63
keijzeg <74183639+keijzeg@users.noreply.github.com>
2021-01-14 15:38:24 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
 // JavaScript Document
 var latestButton = document.querySelector('#latestknop') ;
 var matchesButton = document.querySelector('#matchesknop');
+var buttonSection = document.querySelector('#buttons');
 
 var news = document.querySelector('#news');
 var matches = document.querySelector('#matches');
+
+const mediaQuery = window.matchMedia('(min-width: 700px)')
+
 
 
 // eventHandler functies
@@ -20,7 +24,29 @@ function weergeefMatches(  ) {
     document.querySelector('#news').classList.add('hidden');
 }
 
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    // Then log the following message to the console
+    console.log('Media Query Matched!');
+    buttonSection.classList.add('hidden');
+    news.classList.remove('hidden');
+    matches.classList.remove('hidden');
+  }
+  else {
+    buttonSection.classList.remove('hidden');
+    matches.classList.add('hidden')
+      
+  }
+}
+
 
 // eventListeners
 latestButton.addEventListener('click', weergeefLatest);
 matchesButton.addEventListener('click', weergeefMatches);
+mediaQuery.addListener(handleTabletChange)
+
+handleTabletChange(mediaQuery)
+
+
+// informatie over javascript media queries komt van https://css-tricks.com/css-media-queries/ //
